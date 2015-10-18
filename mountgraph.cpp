10d48bd9c9d1aab58adbcd8d69b5452331f9252c
renyuneyun <renyuneyun@gmail.com>
2015-10-18 11:29:53 +0800

    移除string

--- mountgraph.cpp
+++ mountgraph.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <cstdlib>
 
 using std::ifstream;
-using std::string;
 using std::cout;
 using std::endl;
 
-#define MAX(a,b) a>b ? a : b
-#define BUFSIZE 80
+#define LLEN 280
+#define ARRSIZE 60
 
-string indent = "  ";
+const char indent[] = "  ";
 
 typedef struct node {
-	string path;
-	string device;
+	char path[LLEN];
+	char device[LLEN];
 	struct node **sub;
 	int subsize;
 } Node;
 
-Node *new_node(string path, string device);
+Node *new_node(const char *path, const char *device);
 void add_sub(Node *node, Node *sub);
 Node *clone_node(Node *node);
-Node *totree(string paths[], int size, string device);
+Node *totree(const char * const *paths, int size, const char *device);
 Node *get_tree(ifstream &fin);
 Node *merge(Node *tree1, Node *tree2); //合併兩路徑相同節點（及其子節點）
-void printtree(Node *node, string pre);
+void printtree(Node *node, const char *pre);
 
 int main(int argc, char *argv[])
 {
-	string filename = "/proc/mounts";
+	const char filename[] = "/proc/mounts";
 	ifstream fin(filename);
 	Node *root=NULL, *node=NULL;
 	root = get_tree(fin);
@@ -44,12 +44,12 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-Node *new_node(string path, string device)
+Node *new_node(const char *path, const char *device)
 {
 	const int size = 100;
 	Node *n = new Node;
-	n->path = path;
-	n->device = device;
+	strcpy(n->path, path);
+	strcpy(n->device, device);
 	n->sub = new Node*[size];
 	n->subsize = 0;
 	return n;
@@ -71,7 +71,7 @@ Node *clone_node(Node *node)
 	return n;
 }
 
-Node *totree(string paths[], int size, string device)
+Node *totree(const char * const *paths, int size, const char *device)
 {
 	Node *root = new_node(paths[0], "");
 	Node *p = root, *q;
@@ -80,32 +80,38 @@ Node *totree(string paths[], int size, string device)
 		add_sub(p, q);
 		p = q;
 	}
-	p->device = device;
+	strcpy(p->device ,device);
 	return root;
 }
 
 Node *get_tree(ifstream &fin)
 {
 	Node *root = NULL;
-	string line;
-	if (getline(fin, line)) {
-		int length = int(line.length());
-		char buf[MAX(length, BUFSIZE)];
-		strcpy(buf, line.c_str());
+	char line[LLEN];
+	if (fin.getline(line, LLEN)) {
+		char buf[strlen(line)+1];
+		strcpy(buf, line);
 		char *path=NULL, *device=NULL;
 		device = strtok(buf, " ");
 		path = strtok(NULL, " ");
-		string paths[100];
+		char **paths;
 		int size = 0;
+		paths = (char **) malloc(sizeof(char)*ARRSIZE);
 		char *ipath=NULL;
-		paths[size++] = "";
+			paths[size] = (char *) malloc(sizeof(char)*LLEN);
+		strcpy(paths[size++], "");
 		ipath = strtok(path, "/");
 		while (ipath != NULL) {
-			paths[size++] = ipath;
+			paths[size] = (char *) malloc(sizeof(char)*LLEN);
+			strcpy(paths[size++], ipath);
 			ipath = strtok(NULL, "/");
 		}
-		paths[0] = "/";
+		strcpy(paths[0], "/");
 		root = totree(paths, size, device);
+		while (size > 0) {
+			free(paths[--size]);
+		}
+		free(paths);
 	}
 	return root;
 }
@@ -114,11 +120,11 @@ Node *merge(Node *tree1, Node *tree2)
 {
 	// tree1->path == tree2->path
 	Node *tree = NULL;
-	if ((tree2->device != "") && (tree1->device != tree2->device)) {
+	if ((strcmp(tree2->device,"")!=0) && (strcmp(tree1->device,tree2->device)!=0)) {
 		tree = clone_node(tree2);
 	} else if (tree1->subsize == 0) {
 		tree = clone_node(tree2);
-		tree->device = tree1->device;
+		strcpy(tree->device, tree1->device);
 	} else {
 		tree = new_node(tree1->path, tree1->device);
 		int l1 = tree1->subsize, l2 = tree2->subsize;
@@ -132,7 +138,7 @@ Node *merge(Node *tree1, Node *tree2)
 			for (int j = 0; j < l2; j++)
 				if (!used2[j]) {
 					t2 = tree2->sub[j];
-					if (t1->path == t2->path) {
+					if (strcmp(t1->path, t2->path) == 0) {
 						add_sub(tree, merge(t1, t2));
 						used2[j] = true;
 						used1flag = true;
@@ -151,22 +157,26 @@ Node *merge(Node *tree1, Node *tree2)
 	return tree;
 }
 
-void printtree(Node *node, string pre)
+void printtree(Node *node, const char *pre)
 {
 	int count = 0;
-	int li = indent.size();
-	for (int i = 0; i < int(pre.size()/2); i++) {
-		if (pre.substr(i*li, li) != indent)
+	int li = strlen(indent);
+	char buf[li+1];
+	buf[li] = '\0';
+	for (int i = 0; i < int(strlen(pre)/2); i++) {
+		strncpy(buf, pre+i*li, li);
+		if (strcmp(buf, indent) != 0)
 			break;
 		count++;
 	}
-	string next = "";
+	char next[LLEN] = "";
 	for (int i = 0; i < count+1; i++)
-		next += indent;
-	if (node->device == "") {
-		string cur = pre + node->path + "/";
+		strcat(next, indent);
+	if (strcmp(node->device, "") == 0) {
+		char cur[LLEN] = "\0";
+		strcat(cur, pre); strcat(cur, node->path); strcat(cur, "/");
 		if (node->subsize == 1) {
-			next = cur;
+			strcpy(next, cur);
 		} else {
 			cout << cur << endl;
 		}
